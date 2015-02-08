ccba2bb6d1afc7cd87d82ae23544df881cf6d459
Andryu <shunsuke.andoh@gmail.com>
2015-02-08 04:26:49 -0500

    インデント修正

--- server/main.go
+++ server/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 var pool = newPool()
 
 func main() {
-  auth := new(Authentication)
-  auth.SetKeyForApp("access_token")
-  fmt.Println(auth.Authenticate("jMI3uIk1j-7PFNGStR9JKrDP8QqZfZ4LFVbZb_0yhK4%3D", "message"))
+	// api 認証
+	auth := new(Authentication)
+	auth.SetKeyForApp("access_token")
+	fmt.Println(auth.Authenticate("jMI3uIk1j-7PFNGStR9JKrDP8QqZfZ4LFVbZb_0yhK4%3D", "message"))
 }
